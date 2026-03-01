31f45100341bdef13830fa1c5aec663fca810d2b
Kawoonchik <sydor.anatolii@gmail.com>
2026-03-01 18:42:00 +0200

    Added 2 lab

--- Labka1/src/Main.java
+++ Labka1/src/Main.java
@@ -1,96 +1,81 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-//        int intLegth =5;
-//        int intWidth = 10;
-//
-//        int intArea = intLegth * intWidth;
-//        int intPerimeter = 2 * (intLegth + intWidth);
-//
-//        System.out.println("Площа прямокутника(інт): " + intArea);
-//        System.out.println("Периметр прямокутника(інт): " + intPerimeter);
-//
-//        double dLegth = 5.5;
-//        double dWidth = 10.2;
-//
-//        double dArea = dLegth * dWidth;
-//        double dPerimeter = 2 * (dLegth + dWidth);
-//
-//        System.out.println("Площа прямокутника(d): " + dArea);
-//        System.out.println("Периметр прямокутника(d): " + dPerimeter);
-//
-//        // 2 завд
-//        int a = 25;
-//        int b = 42;
-//
-//        if (a>b) {
-//            System.out.println("Найбільше число: " + a);
-//        } else if (b>a) {
-//            System.out.println("Найбільше число: " + b);
-//        } else {
-//            System.out.println("Одинакові: " + a);
-//        }
+    static void main(String[] args) {
+        double celsius = 25.5;
+
+        double fahrenheit = (celsius * 9.0 / 5.0) + 32.0;
+
+        System.out.println(celsius + " градусів цельсія — це " + fahrenheit + " градусів Фаренгейта");
+
+        // 2 завд
+        int a = 25;
+        int b = 42;
+
+        if (a>b) {
+            System.out.println("Найбільше число: " + a);
+        } else if (b>a) {
+            System.out.println("Найбільше число: " + b);
+        } else {
+            System.out.println("Одинакові: " + a);
+        }
 
         // 3 завд
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Введіть 1 число:");
-//        double num1 = scanner.nextDouble();
-//
-//        System.out.println("Введіть операцію(+ - * /)");
-//        char operator = scanner.next().charAt(0);
-//
-//        System.out.println("Введіть 2 число:");
-//        double num2 = scanner.nextDouble();
-//
-//        double result = 0;
-//        boolean isValid = true;
-//
-//        switch (operator) {
-//            case '+':
-//                result = num1 + num2;
-//                break;
-//            case '-':
-//                result = num1 - num2;
-//                break;
-//            case '*':
-//                result = num1 * num2;
-//                break;
-//            case '/':
-//                if (num2 !=0) {
-//                    result = num1 / num2;
-//                } else {
-//                    System.out.println("Помилка, на 0 не ділиться");
-//                }
-//                break;
-//            default:
-//                System.out.println("невідома операція");
-//                isValid = false;
-//        }
-//        if (isValid) {
-//            System.out.println("Результат:" + result);
-//        }
+
+
+        double num1 = 4;
+
+        char operator = '*';
+
+        double num2 = 6;
+
+        double result = 0;
+        boolean isValid = true;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 !=0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Помилка, на 0 не ділиться");
+                }
+                break;
+            default:
+                System.out.println("невідома операція");
+                isValid = false;
+        }
+        if (isValid) {
+            System.out.println("Результат:" + result);
+        }
 
         // завд 4
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Введіть число");
-//        int number = scanner.nextInt();
-//
-//        if (number < 0) {
-//            System.out.println("Число відємне");
-//        } else {
-//            long factorial = 1;
-//            int i = 1;
-//
-//            while (i <= number) {
-//                factorial = factorial * i;
-//                i++;
-//            }
-//
-//            System.out.println("Факторіл з　" + number + "! дорівнює:" + factorial);
-//        }
+
+
+
+        int number = 4;
+
+        if (number < 0) {
+            System.out.println("Число відємне");
+        } else {
+            long factorial = 1;
+            int i = 1;
+
+            while (i <= number) {
+                factorial = factorial * i;
+                i++;
+            }
+
+            System.out.println("Факторіл з　" + number + "! дорівнює:" + factorial);
+        }
 
         // 任务 5
 
---
+++ Labka2/Lab2/src/Main.java
@@ -0,0 +1,49 @@
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
+import java.util.Queue;
+
+class ServerUnavailableException extends Exception {
+    public ServerUnavailableException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+
+
+    public static void sendEmail(String email, boolean isServerUp) throws ServerUnavailableException {
+        if (!isServerUp) {
+            throw new ServerUnavailableException("Помилка відправки: Сервер пошти недоступний для листа " + email);
+        }
+        System.out.println("Успіх: Лист на адресу " + email + " успішно надіслано!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("=== Система відправки листів ===");
+
+        Queue<String> emailQueue = new LinkedList<>();
+
+        emailQueue.add("boss@company.com");
+        emailQueue.add("client@domain.com");
+        emailQueue.add("friend@mail.com");
+
+        boolean isServerUp = false;
+
+        while (true) {
+            try {
+                String currentEmail = emailQueue.remove();
+                System.out.println("\nВзято з черги: " + currentEmail);
+
+                try {
+                    sendEmail(currentEmail, isServerUp);
+                } catch (ServerUnavailableException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            } catch (NoSuchElementException e) {
+                System.out.println("\nУвага: Черга порожня! Спроба взяти лист з порожньої черги перехоплена.");
+                break;
+            }
+        }
+    }
+}
